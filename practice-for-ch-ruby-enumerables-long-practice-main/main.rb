132870b280f3c53c2bd48dba105cf73d7ebc2350
Wizzerrd <luis@DESKTOP-7AS3T51>
2023-06-05 14:35:55 -0700

    Add my_any, Add my_all, add my_flatten

--- practice-for-ch-ruby-enumerables-long-practice-main/main.rb
+++ practice-for-ch-ruby-enumerables-long-practice-main/main.rb
@@ -13,6 +13,46 @@ class Array
         end
         self
     end
+
+    def my_reject(&prc)
+        res = []
+        self.each { |i| res << i if !prc.call(i) }
+        res
+    end
+
+    def my_any?(&prc)
+        self.each {|ele| return true if yield(ele)}
+        false
+    end
+
+    def my_all?(&prc)
+        self.each {|ele| return false if !yield(ele)}
+        true
+    end
+
+    def my_flatten 
+        return [self] if !self.is_a? Array 
+        return self if self.none? {|ele| ele.is_a? Array}
+        return self if self.none? {|ele| ele.is_a? Array}
+            new_arr = []
+            self.each do |ele|
+                if !ele.is_a? Array
+                    new_arr << ele
+                else
+                    ele.each {|ele2| new_arr << ele2}
+                end
+            end
+            new_arr.my_flatten 
+    end
+
+    def my_zip(*arr)
+        max = 0 
+        arr.each {|ele| max = ele.length if ele.length > max}
+
+        new_arr = Array.new {Array.new(max)}
+        (0...arr.length).each
+    end
+    
 end
 
 # calls my_each twice on the array, printing all the numbers twice.
@@ -28,22 +68,32 @@ return_value = [1, 2, 3].my_each do |num|
    #    2
    #    3
    
-   p return_value  # => [1, 2, 3]
+#    p return_value  # => [1, 2, 3]
+puts "////////"
 
 
-# a = [1, 2, 3]
-# p a.my_select { |num| num > 1 } # => [2, 3]
-# p a.my_select { |num| num == 4 } # => []
+a = [1, 2, 3]
 
-# a = [1, 2, 3]
-# p a.my_reject { |num| num > 1 } # => [1]
-# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
+puts "////////"
 
-# a = [1, 2, 3]
-# p a.my_any? { |num| num > 1 } # => true
-# p a.my_any? { |num| num == 4 } # => false
-# p a.my_all? { |num| num > 1 } # => false
-# p a.my_all? { |num| num < 4 } # => true
+
+a = [1, 2, 3]
+p a.my_reject { |num| num > 1 } # => [1]
+p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+puts "////////"
+
+
+a = [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
+puts "////////"
+
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+puts "////////"
 
 # a = [ 4, 5, 6 ]
 # b = [ 7, 8, 9 ]
